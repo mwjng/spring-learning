b9c4661e265b32276d61c5cefc27f1d3c2c6ddb9
mwjng <87010483+mwjng@users.noreply.github.com>
2023-08-19 05:57:54 +0900

    스프링 핵심원리

---
+++ core/src/main/java/com/example/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,21 @@
+package com.example.core.discount;
+
+import com.example.core.member.Grade;
+import com.example.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000; // 1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
